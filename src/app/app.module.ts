356773d4391ab05f65bba5b13c560f0b4516dc13
0xSzeth <0xSzeth@gmail.com>
2021-08-23 11:00:21 -0600

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { IceCREAMComponent } from './ice-cream/ice-cream.component';
 import { EthV1Component } from './eth-v1/eth-v1.component';
 import { IronBankComponent } from './iron-bank/iron-bank.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IceCREAMComponent,
     EthV1Component,
-    IronBankComponent
+    IronBankComponent,
+    HeaderComponent
   ],
   imports: [
     NgbModule,
