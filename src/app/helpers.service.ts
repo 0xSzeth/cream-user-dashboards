60010247c852b370e6a4a7db16bc38be68c57c52
0xSzeth <0xSzeth@gmail.com>
2021-08-23 16:14:03 -0600

    Implement token price queries

--- src/app/helpers.service.ts
+++ src/app/helpers.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class HelpersService {
 
   constructor() { }
+
+  async getTokenPriceUSD(address: string): Promise<number> {
+    const apiStr = `https://api.coingecko.com/api/v3/coins/ethereum/contract/${address}/market_chart/?vs_currency=usd&days=0`;
+    const rawResult = await this.httpsGet(apiStr, 300);
+    return rawResult.prices[0][1];
+  }
+
+  async getHistoricalTokenPriceUSD(address: string, days: string): Promise<Array<any>> {
+    const apiStr = `https://api.coingecko.com/api/v3/coins/ethereum/contract/${address}/market_chart/?vs_currency=usd&days=${days}`;
+    const rawResult = await this.httpsGet(apiStr, 300);
+    return rawResult.prices;
+  }
+
+  async httpsGet(apiStr: string, cacheMaxAge: number = 60) {
+    const request = await fetch(apiStr, {
+      headers: { 'Cache-Control': `max-age=${cacheMaxAge}` },
+    });
+    return await request.json();
+  }
+
+
 }
