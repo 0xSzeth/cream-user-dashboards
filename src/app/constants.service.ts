4033b030ef5e7ea0f6bd504b4f238f47f0794d73
0xSzeth <0xSzeth@gmail.com>
2021-08-23 16:14:49 -0600

    Add polygon graphql endpoint

--- src/app/constants.service.ts
+++ src/app/constants.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConstantsService {
+  // tokens
+  CREAM = "0x2ba592f78db6436527729929aaf6c908497cb200";
+  ICE_CREAM = "0x3986425b96F11972d31C78ff340908832C5c0043";
 
-  constructor() { }
+  // subgraph endpoints
+  GRAPHQL_POLYGON = "https://api.thegraph.com/subgraphs/name/creamfinancedev/cream-portfolio-polygon";
+
+  // utils
+  PRECISION = 1e18;
 }
