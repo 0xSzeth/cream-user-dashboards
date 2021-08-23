3608381f64a75224f24c51310c1d52064930c6b0
0xSzeth <0xSzeth@gmail.com>
2021-08-23 10:43:05 -0600

    Update the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IceCREAMComponent } from './ice-cream/ice-cream.component';
+import { EthV1Component } from './eth-v1/eth-v1.component';
+import { IronBankComponent } from './iron-bank/iron-bank.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'iceCREAM',
+    component: IceCREAMComponent,
+  },
+  {
+    path: 'ethv1',
+    component: EthV1Component,
+  },
+  {
+    path: 'ironBank',
+    component: IronBankComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IceCREAMComponent } from './ice-cream/ice-cream.component';
+import { EthV1Component } from './eth-v1/eth-v1.component';
+import { IronBankComponent } from './iron-bank/iron-bank.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IceCREAMComponent
+    IceCREAMComponent,
+    EthV1Component,
+    IronBankComponent
   ],
   imports: [
     BrowserModule,
