d0bc51bfd53f63385b304dc1773b4e7d51541c12
0xSzeth <0xSzeth@gmail.com>
2021-08-23 11:12:24 -0600

    Implement basic navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { IceCREAMComponent } from './ice-cream/ice-cream.component';
 import { EthV1Component } from './eth-v1/eth-v1.component';
 import { IronBankComponent } from './iron-bank/iron-bank.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
   {
     path: 'iceCREAM',
     component: IceCREAMComponent,
