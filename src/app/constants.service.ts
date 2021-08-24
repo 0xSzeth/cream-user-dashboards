e4c109282ce6463e224a37da19a6091fb6e8a509
0xSzeth <0xSzeth@gmail.com>
2021-08-24 12:36:59 -0600

    Add graphql endpoints

--- src/app/constants.service.ts
+++ src/app/constants.service.ts
@@ -9,7 +9,15 @@ export class ConstantsService {
   ICE_CREAM = "0x3986425b96F11972d31C78ff340908832C5c0043";
 
   // subgraph endpoints
+  GRAPHQL_MAINNET = "https://api.thegraph.com/subgraphs/name/creamfinancedev/cream-portfolio-mainnet";
+  GRAPHQL_IRONBANK = "https://api.thegraph.com/subgraphs/name/creamfinancedev/cream-portfolio-ironbank";
   GRAPHQL_POLYGON = "https://api.thegraph.com/subgraphs/name/creamfinancedev/cream-portfolio-polygon";
+  GRAPHQL_FANTOM = "https://api.thegraph.com/subgraphs/name/creamfinancedev/cream-portfolio-fantom";
+  GRAPHQL_BSC = "https://api.thegraph.com/subgraphs/name/creamfinancedev/cream-portfolio-bsc";
+
+  GRAPHQL_UNISWAP_V2 = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2';
+  GRAPHQL_UNISWAP_V3 = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3';
+  GRAPHQL_SUSHISWAP = 'https://api.thegraph.com/subgraphs/name/sushiswap/exchange';
 
   // utils
   PRECISION = 1e18;
