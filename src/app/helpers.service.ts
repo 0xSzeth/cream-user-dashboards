71dd2ea88d6dfa09cc0f0417ba012e5e73d07fd5
0xSzeth <0xSzeth@gmail.com>
2021-08-26 15:33:58 -0600

    Update helper service to support polygon asset price query

--- src/app/helpers.service.ts
+++ src/app/helpers.service.ts
@@ -1,16 +1,75 @@
 import { Injectable } from '@angular/core';
+import { ConstantsService } from './constants.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelpersService {
 
-  constructor() { }
+  constructor(
+    public constants: ConstantsService,
+  ) { }
 
-  async getTokenPriceUSD(address: string): Promise<number> {
+  // coingecko
+  async getTokenPriceUSD(address: string, chainID: number): Promise<number> {
+
+    console.log(address);
+
+    if (address.toLowerCase() === this.constants.AAVE[chainID].toLowerCase()) {
+      address = this.constants.AAVE[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.CREAM[chainID].toLowerCase()) {
+      address = this.constants.CREAM[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.CRV[chainID].toLowerCase()) {
+      address = this.constants.CRV[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.DAI[chainID].toLowerCase()) {
+      address = this.constants.DAI[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.DPI[chainID].toLowerCase()) {
+      address = this.constants.DPI[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.FXS[chainID].toLowerCase()) {
+      address = this.constants.FXS[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.IBBTC[chainID].toLowerCase()) {
+      address = this.constants.IBBTC[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.LINK[chainID].toLowerCase()) {
+      address = this.constants.LINK[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.QUICK[chainID].toLowerCase()) {
+      address = this.constants.QUICK[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.SNX[chainID].toLowerCase()) {
+      address = this.constants.SNX[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.SUSHI[chainID].toLowerCase()) {
+      address = this.constants.SUSHI[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.UNI[chainID].toLowerCase()) {
+      address = this.constants.UNI[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.USDC[chainID].toLowerCase()) {
+      address = this.constants.USDC[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.USDT[chainID].toLowerCase()) {
+      address = this.constants.USDT[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.WBTC[chainID].toLowerCase()) {
+      address = this.constants.WBTC[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.WETH[chainID].toLowerCase()) {
+      address = this.constants.WETH[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    } else if (address.toLowerCase() === this.constants.WMATIC[chainID].toLowerCase()) {
+      address = this.constants.WMATIC[this.constants.CHAIN_ID.MAINNET].toLowerCase();
+    }
+
     const apiStr = `https://api.coingecko.com/api/v3/coins/ethereum/contract/${address}/market_chart/?vs_currency=usd&days=0`;
     const rawResult = await this.httpsGet(apiStr, 300);
     return rawResult.prices[0][1];
+  }
+
+  // 1inch
+  async getTokenPriceUSD2(from: string, to: string, amount: number): Promise<number> {
+    //const apiStr = `https://api.coingecko.com/api/v3/coins/ethereum/contract/${address}/market_chart/?vs_currency=usd&days=0`;
+    const apiStr = `https://api.1inch.exchange/v3.0/1/quote?fromTokenAddress=${from}&toTokenAddress=${to}&amount=${amount}`;
+    const rawResult = await this.httpsGet(apiStr, 300);
+    if (rawResult.error) {
+      console.log("here's an error!!!!!!!!")
+      return 0;
+    } else {
+      console.log(rawResult);
+      return rawResult.prices[0][1];
+    }
+
+
   }
 
   async getHistoricalTokenPriceUSD(address: string, days: string): Promise<Array<any>> {
@@ -19,7 +78,7 @@ export class HelpersService {
     return rawResult.prices;
   }
 
-  async httpsGet(apiStr: string, cacheMaxAge: number = 60) {
+  async httpsGet(apiStr: string, cacheMaxAge: number = 3) {
     const request = await fetch(apiStr, {
       headers: { 'Cache-Control': `max-age=${cacheMaxAge}` },
     });
